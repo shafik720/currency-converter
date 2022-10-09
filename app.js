5455e828e63ecdcaf42085c375f39e1afe55d315
shafik720 <shafikrasel5@gmail.com>
2022-10-09 12:16:19 +0600

    sdfsdf

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 let droplist = document.querySelectorAll('.selection-div select');
 
 for(let i=0; i<droplist.length; i++){
+    let selected;
     for(currencyCode in country_list){
-        let optionTag = `<option value="${currencyCode}">${currencyCode}</option>`;
+        if(i==0){
+            selected = currencyCode == 'USD' ? 'selected' : '';
+        }else if(i==1){
+            selected = currencyCode == 'BDT' ? 'selected' :  '';
+        }
+        let optionTag = `<option value="${currencyCode}" ${selected}>${currencyCode}</option>`;
         droplist[i].insertAdjacentHTML('beforeend',optionTag);
     }
+    console.log(i);
 }
