5490b494c6555eaf0518000f161d00feb205af4d
shafik720 <shafikrasel5@gmail.com>
2022-10-10 19:53:38 +0600

    dfgfsdg

--- app2.js
+++ app2.js
@@ -1,6 +1,10 @@
 
-const selectionList = document.querySelectorAll('.selection-div select')
-
+const selectionList = document.querySelectorAll('.selection-div select'),
+button = document.querySelector('.exchange-rate  button'),
+to = document.querySelector('.selection-div .to select'),
+from = document.querySelector('.selection-div .from select'),
+inputAmount = document.querySelector('.inter-amount input'),
+resultText = document.querySelector('#result')
 ;
 
 for(i=0;i<selectionList.length;i++){
@@ -33,4 +37,24 @@ function loadFlag(any){
             any.parentElement.querySelector('img').src = `https://flagcdn.com/48x36/${country_list[currencyCode].toLowerCase()}.png`
         }
     }
+}
+
+button.addEventListener('click',()=>{    
+    resultText.innerText = 'Getting Results ......';
+    let url = `https://v6.exchangerate-api.com/v6/4e52c37ac3dc20f13a920cef/latest/${from.value}`
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>showExchange(data));
+})
+
+function showExchange(element){
+    
+    if(inputAmount.value == '' || inputAmount.value == 0){
+        inputAmount.value = 1 ;
+    }
+    let totalAmount = (inputAmount.value * element.conversion_rates[to.value]).toFixed(2);
+
+    resultText.innerText = ` ${inputAmount.value} ${from.value} = ${totalAmount} ${to.value}`;
+    // console.log(element.conversion_rates[to.value]);
 }
