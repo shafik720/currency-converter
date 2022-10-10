1647f12a6eecd750b2b19aa3c1ebdc25c7d29ef1
shafik720 <shafikrasel5@gmail.com>
2022-10-10 17:02:58 +0600

    dfsgsfd

--- app2.js
+++ app2.js
@@ -1,25 +1,39 @@
 
-const selectionMenu = document.querySelectorAll('.selection-div select');
+const selectionList = document.querySelectorAll('.selection-div select'),
+img = document.querySelector('.selection-div img')
+;
 
-for(let i=0; i<selectionMenu.length;i++){
+
+for(let i=0; i<selectionList.length;i++){
+    let option;
     let selected;
-    for(let currencyCodes in country_list){
+    for(let currencyCode in country_list){
         if(i==0){
-            if(currencyCodes == 'USD'){
-                selected = 'selected';
-            }else{
-                selected = '';
-            }
-        }else if(i==1){
-            if(currencyCodes == 'BDT'){
+            if(currencyCode == 'USD'){
                 selected = 'selected';
             }else{
                 selected = '';
             }
         }
-
-        let option = `<option ${selected} >${currencyCodes}</option>`
-        selectionMenu[i].insertAdjacentHTML('beforeend', option);
-        
+        if(i==1){
+            if(currencyCode == 'BDT'){
+                selected = 'selected' ;
+            }else{
+                selected = '';
+            }
+        }
+        option = `<option ${selected}>${currencyCode}</option>`;
+        selectionList[i].insertAdjacentHTML('beforeend',option);
+        selectionList[i].addEventListener('change',(e)=>{
+            loadFlag(e.target);
+        })
+    }
+}
+function loadFlag(any){
+    // console.log(any.value);
+    for(let currencyCode in country_list){
+        if(currencyCode == any.value){
+            any.parentElement.querySelector('img').src = `https://flagcdn.com/48x36/${country_list[currencyCode].toLowerCase()}.png`;
+        }
     }
 }
